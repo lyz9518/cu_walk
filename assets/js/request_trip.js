28706b2860b9d181829bfe547ba2bcdd054caef5
gefeiniangniang <72155998+gefeiniangniang@users.noreply.github.com>
2021-12-02 21:48:47 -0500

    o

---
+++ assets/js/request_trip.js
@@ -0,0 +1,28 @@
+// Glabal Var
+var selected_group_name = null;
+
+function select_one(checkbox) {
+    var checkboxes = document.getElementsByName("groups")
+    checkboxes.forEach((item) => {
+        if (item !== checkbox){
+            item.checked = false
+        } else{
+            selected_group_name = item.value;
+        }
+
+    })
+    
+}
+
+// TODO: Implement API call
+function select_group(){
+    if (selected_group_name == null){
+        alert("Please select a group to continues")
+    } else {
+        // TODO: API Call here
+        console.log(selected_group_name);
+
+        // Jump to ready page
+        window.location.href = "ready.html";
+    }
+}
