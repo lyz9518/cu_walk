5bde10fb2492b3273b6ef61e0738cb5c0a461ec9
Arthur Liu <lyz9518@163.com>
2021-12-03 00:12:10 -0500

    dynamic list

--- assets/js/select_trip.js
+++ assets/js/select_trip.js
@@ -1,6 +1,31 @@
 // Glabal Var
 var selected_group_name = null;
 
+function load_groups(){
+    // TODO: Get Group Info API
+    var group_form = document.getElementById("group_form");
+
+    // //TODO: pseudo-code now
+    // for (i=0; i<3; i++){
+    //     // Create an <input> element, set its type and name attributes
+    //     var input = document.createElement("input");
+    //     var label = document.createElement("label");
+    //     input.type = "checkbox";
+    //     input.id = "group_"+i;
+    //     input.name = "groups";
+    //     input.value = "group_"+i;
+    //     label.for = "group_"+i;
+    //     container.appendChild(input);
+    //     container.appendChild(label);
+    //     // Append a line break 
+    //     container.appendChild(document.createElement("br"));
+    //     container.appendChild(document.createElement("br"));
+    // }
+    
+}
+
+
+
 function select_one(checkbox) {
     var checkboxes = document.getElementsByName("groups")
     checkboxes.forEach((item) => {
