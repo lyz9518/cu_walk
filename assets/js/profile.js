618e7b3333ddcde2a499ecdac1b857e7aa7c6c34
Arthur Liu <lyz9518@163.com>
2021-12-02 20:42:24 -0500

    add ajax lib

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -10,3 +10,27 @@ function update_profile(){
     console.log(phone_num);
     console.log(emergency_contact);
 }
+
+function load_existing_profile(){
+    var existing_user = false;
+    var nickname = "abc";
+    var gender = "Male";
+    var phone_num = "0123456789";
+    var emergency_contact = "9876543210";
+    // TODO: Profile Get API Here
+    // Assign the vars above.
+
+
+    if (existing_user == false){
+        document.getElementById("nickname").placeholder = "";
+        document.getElementById("gender").placeholder = "";
+        document.getElementById("phone_num").placeholder = "";
+        document.getElementById("emergency_contact").placeholder = "";
+    } else {
+        
+        document.getElementById("nickname").placeholder = nickname;
+        document.getElementById("gender").placeholder = gender;
+        document.getElementById("phone_num").placeholder = phone_num;
+        document.getElementById("emergency_contact").placeholder = emergency_contact;
+    }
+}
