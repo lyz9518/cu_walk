69647887431abb2fd6e913b2c9791d4ae16f780e
gefeiniangniang <72155998+gefeiniangniang@users.noreply.github.com>
2021-12-03 21:47:06 -0500

    geolocation

--- assets/js/request_trip.js
+++ assets/js/request_trip.js
@@ -2,7 +2,6 @@
 var time = null;
 var departure = null;
 var destination = null;
-
 // TODO: Implement API call
 function request_trip(){
     time = document.getElementById("time").value;
@@ -15,15 +14,29 @@ function request_trip(){
     } else if(destination == null || destination == "" ){
         alert("Please type in a destination")
     } else {
-        // TODO: API Call here
-        console.log(time);
-        console.log(departure);
-        console.log(destination);
-        
-        // Jump to ready page
-        window.location.href = "select_trip.html";
+        var address = destination.replaceAll(" ", "+");
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+",+New+York,+NY&key=AIzaSyAOhQqll5NI0TUOrYbbvQbF-TdU9qYWJHM";
+        console.log(url)
+        $.ajax({
+            url: url,
+            success: function(data) {
+                var result = data.results[0];
+                var lat = result.geometry.location.lat;
+                var lng = result.geometry.location.lng;
+                console.log(lat);
+                console.log(lng);
+                console.log(time);
+                console.log(departure);
+                console.log(destination);
+
+
+                window.location.href = "select_trip.html"
+            }
+        });
     }
 }
+
+
 
 function edit_profile(){
     window.location.href = "profile.html";
