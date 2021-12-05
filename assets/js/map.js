7876af165df7802821038605b538f1c92db9ed1c
CrazyQie <728977862@qq.com>
2021-12-04 21:12:42 -0500

    update map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,84 +1,122 @@
 // https://stackoverflow.com/questions/5865089/simple-javascript-loop-that-repeats-each-second/28107143
+var idToken = localStorage.getItem("idToken");
+var accessToken = localStorage.getItem("accessToken");
 
-var from_location = {lat: 40.8114424, lng: -73.9587781};
-var to_location = {lat: 40.8105964, lng: -73.95830099999999};
-var myLatLng = from_location;
+$.ajax({
+    url: "https://k9wj046mrd.execute-api.us-east-1.amazonaws.com/6998FirstTry/group" + "?accessToken=" + accessToken,
+    headers: {"Token": idToken},
+    type: 'GET',
+    cache: false,
+    processData: false,
+    contentType: 'application/json',
+    success: function (r) {
+        //TODO: debug the logic here and need to optimze the structure here.
+        alert("Starting Trip!");
+        let res = JSON.parse(JSON.stringify(r));
+        // from_location_str is the location name of departure.
+        let from_location_str = res[0]['departure'];
+        // to_location_str is a dict
+        let to_location_str = res[0]['first_user_coordinate'];
+        let address = from_location_str.replaceAll(" ", "+");
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + ",+New+York,+NY&key=AIzaSyAOhQqll5NI0TUOrYbbvQbF-TdU9qYWJHM";
+        $.ajax({
+            url: url,
+            success: function (data) {
+                let result = data.results[0];
+                let latCord = result.geometry.location.lat;
+                let lngCord = result.geometry.location.lng;
+                // in order to get marker, from_location is in coordicnate format.
+                let from_location = {"lat": latCord, "lng": lngCord};
+                // to_location should be in location name, not coordinate
+                let to_location = to_location_str["address"];
 
-var mapOptions = {
-    // center: myLatLng,
-    zoom: 18, mapTypeId: google.maps.MapTypeId.ROADMAP, center: myLatLng
-};
+                console.log(to_location);
+                // test location
+                // var from_location = {lat: 40.8114424, lng: -73.9587781};
+                // var to_location = {lat: 40.8105964, lng: -73.95830099999999};
 
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-var directionsService = new google.maps.DirectionsService();
-var directionsDisplay = new google.maps.DirectionsRenderer();
-directionsDisplay.setMap(map);
+                var myLatLng = from_location;
 
-var request = {
-    origin: from_location, destination: to_location, travelMode: google.maps.TravelMode.WALKING, //WALKING, BYCYCLING, TRANSIT
-    unitSystem: google.maps.UnitSystem.IMPERIAL
-}
+                var mapOptions = {
+                    // center: myLatLng,
+                    zoom: 18, mapTypeId: google.maps.MapTypeId.ROADMAP, center: myLatLng
+                };
 
-//pass the request to the route method
-directionsService.route(request, function (response, status) {
-    // directionsDisplay.setDirections(result);
+                var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+                var directionsService = new google.maps.DirectionsService();
+                var directionsDisplay = new google.maps.DirectionsRenderer();
+                directionsDisplay.setMap(map);
 
-    new google.maps.DirectionsRenderer({
-        map: map,
-        directions: response,
-        suppressMarkers: true
-    });
-    var leg = response.routes[0].legs[0];
-    new google.maps.Marker({position: leg.start_location, icon: icons.start, title: "title", map: map})
-    new google.maps.Marker({position: leg.end_location, icon: icons.end, title: "title", map: map})
-});
+                var request = {
+                    origin: from_location, destination: to_location, travelMode: google.maps.TravelMode.WALKING, //WALKING, BYCYCLING, TRANSIT
+                    unitSystem: google.maps.UnitSystem.IMPERIAL
+                }
 
-var icons = {
-    start: new google.maps.MarkerImage(
-        'http://maps.google.com/mapfiles/ms/micons/blue.png',
-        new google.maps.Size(44, 32),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(22, 32)),
-    end: new google.maps.MarkerImage(
-        'http://maps.google.com/mapfiles/ms/micons/red.png',
-        new google.maps.Size(44, 32),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(22, 32))
-};
+                //pass the request to the route method
+                directionsService.route(request, function (response, status) {
+                    // directionsDisplay.setDirections(result);
 
-var lat1;
-var lng1;
+                    new google.maps.DirectionsRenderer({
+                        map: map,
+                        directions: response,
+                        suppressMarkers: true
+                    });
+                    var leg = response.routes[0].legs[0];
+                    new google.maps.Marker({position: leg.start_location, icon: icons.start, title: "title", map: map})
+                    new google.maps.Marker({position: leg.end_location, icon: icons.end, title: "title", map: map})
+                });
 
-// const image =
-//     "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+                var icons = {
+                    start: new google.maps.MarkerImage(
+                        'http://maps.google.com/mapfiles/ms/micons/blue.png',
+                        new google.maps.Size(44, 32),
+                        new google.maps.Point(0, 0),
+                        new google.maps.Point(22, 32)),
+                    end: new google.maps.MarkerImage(
+                        'http://maps.google.com/mapfiles/ms/micons/red.png',
+                        new google.maps.Size(44, 32),
+                        new google.maps.Point(0, 0),
+                        new google.maps.Point(22, 32))
+                };
 
-var marker = new google.maps.Marker({
-    // position: myLatLng, map, icon: image
-    position: myLatLng, map
-});
+                var lat1;
+                var lng1;
 
-if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showPosition);
-} else {
-    alert("Geolocation is not supported by this browser.");
-}
+                // const image =
+                //     "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
 
-function showPosition(position) {
-    // x.innerHTML="Latitude: " + position.coords.latitude + 
-    // "<br>Longitude: " + position.coords.longitude;
-    lat1 = position.coords.latitude;
-    lng1 = position.coords.longitude;
-    myLatLng = {lat: lat1, lng: lng1};
-    marker.setPosition(myLatLng);
-}
+                var marker = new google.maps.Marker({
+                    // position: myLatLng, map, icon: image
+                    position: myLatLng, map
+                });
 
-console.log(idToken);
-console.log(accessToken);
+                if (navigator.geolocation) {
+                    navigator.geolocation.watchPosition(showPosition);
+                } else {
+                    alert("Geolocation is not supported by this browser.");
+                }
+
+                function showPosition(position) {
+                    // x.innerHTML="Latitude: " + position.coords.latitude +
+                    // "<br>Longitude: " + position.coords.longitude;
+                    lat1 = position.coords.latitude;
+                    lng1 = position.coords.longitude;
+                    myLatLng = {lat: lat1, lng: lng1};
+                    marker.setPosition(myLatLng);
+                }
+
+                console.log(idToken);
+                console.log(accessToken);
+
+
+            }
+        });
+    }
+})
+
 
 function emergency() {
     console.log("call emergency contact");
-    var idToken = localStorage.getItem("idToken");
-    var accessToken = localStorage.getItem("accessToken");
     $.ajax({
         url: "https://k9wj046mrd.execute-api.us-east-1.amazonaws.com/6998FirstTry/emergency" + "?accessToken=" + accessToken,
         headers: {"Token": idToken},
@@ -94,8 +132,6 @@ function emergency() {
 
 function arrived() {
     console.log("arrived home");
-    var idToken = localStorage.getItem("idToken");
-    var accessToken = localStorage.getItem("accessToken");
     $.ajax({
         url: "https://k9wj046mrd.execute-api.us-east-1.amazonaws.com/6998FirstTry/arrive" + "?accessToken=" + accessToken,
         headers: {"Token": idToken},
--- assets/js/request_trip.js
+++ assets/js/request_trip.js
@@ -10,23 +10,23 @@ var lat = null;
 var lng = null;
 
 
-function request_trip(){
+function request_trip() {
     time = document.getElementById("time").value;
     departure = document.getElementById("departure").value;
     destination = document.getElementById("destination").value;
-    if (time == null || time == "" ){
+    if (time == null || time === "") {
         alert("Please select a time")
-    } else if(departure == null || departure == "" ){
+    } else if (departure == null || departure === "") {
         alert("Please select a departure building")
-    } else if(destination == null || destination == "" ){
+    } else if (destination == null || destination === "") {
         alert("Please type in a destination")
     } else {
         var address = destination.replaceAll(" ", "+");
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+",+New+York,+NY&key=AIzaSyAOhQqll5NI0TUOrYbbvQbF-TdU9qYWJHM";
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + ",+New+York,+NY&key=AIzaSyAOhQqll5NI0TUOrYbbvQbF-TdU9qYWJHM";
         console.log(url)
         $.ajax({
             url: url,
-            success: function(data) {
+            success: function (data) {
                 var result = data.results[0];
                 lat = result.geometry.location.lat;
                 lng = result.geometry.location.lng;
@@ -36,56 +36,52 @@ function request_trip(){
                 console.log(departure);
                 console.log(destination);
                 console.log("Google API finished");
+                var body = JSON.stringify({
+                    "time": time,
+                    "departure": departure,
+                    "destination": {
+                        "address": destination,
+                        "latitude": lat,
+                        "longitude": lng
+                    }
+                })
+
+                // TODO: Add real data to body here
+                var body = JSON.stringify({
+                    "time": time,
+                    "departure": departure,
+                    "destination": {
+                        "address": destination,
+                        "latitude": lat,
+                        "longitude": lng
+                    }
+                })
+
+                var API_addr = "https://k9wj046mrd.execute-api.us-east-1.amazonaws.com/6998FirstTry/trip";
+
+                $.ajax({
+                    url: API_addr + "?accessToken=" + accessToken,
+                    headers: {"Token": idToken},
+                    type: 'POST',
+                    data: body,
+                    cache: false,
+                    processData: false,
+                    contentType: 'application/json',
+                    success: function (r) {
+                        console.log(r);
+                        alert("Trip Requested!");
+                    }
+                })
+
+                // TODO: uncomment the jump page code when others are ready
+                window.location.assign("select_trip.html");
             }
         });
     }
-
-    setTimeout(function (){
-        var body = JSON.stringify({
-            "time": time,
-            "departure": departure,
-            "destination": {
-                "address": destination,
-                "latitude": lat, 
-                "longitude": lng
-            }
-        })
-
-        // TODO: Add real data to body here
-        var body = JSON.stringify({
-            "time": time,
-            "departure": departure,
-            "destination": {
-                "address": destination,
-                "latitude": lat, 
-                "longitude": lng
-            }
-        })
-
-        var API_addr = "https://k9wj046mrd.execute-api.us-east-1.amazonaws.com/6998FirstTry/trip";
-
-        $.ajax({
-            url: API_addr + "?accessToken=" + accessToken,
-            headers: {"Token": idToken},
-            type: 'POST',
-            data: body,
-            cache: false,
-            processData: false,
-            contentType: 'application/json',
-            success: function (r) {
-                console.log(r);
-                alert("Trip Requested!");
-            }
-        })
-
-        // TODO: uncomment the jump page code when others are ready 
-        window.location.assign("select_trip.html");
-    }, 3000);
 }
 
 
-
-function edit_profile(){
+function edit_profile() {
     // window.location.href = "profile.html";
     window.location.assign("profile.html");
 }
